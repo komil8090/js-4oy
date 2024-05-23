7d26b1e0dd13f97dc0339986fab13b45db4b027b
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-05-23 11:00:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let ism = prompt("Ismingizni kiriting")
+
+let fam = prompt("Familiyangizni kiriting")
+
+let age = prompt("Yoshingizni kiriting")
+
+let man = prompt("Manzilingizni kiriting")
+
+let fan = prompt("Yoqtirgan faningizni kiriting")
+
+console.log("ismi" ,ism)
+
+console.log("familiyasi" ,fam)
+
+console.log("yoshi" ,age)
+
+console.log("manzil" ,man)
+
+console.log("yoqtirgan fani" ,fan)
